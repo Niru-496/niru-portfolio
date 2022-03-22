c514617b07122398165ad57db66dae2361ff64db
Niranjan <narayana9492324464@gmail.com>
2022-03-22 10:44:33 +0530

    link

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,43 +6,44 @@ import "./typing.css";
 
 const Banner = () => {
   return (
-    <Container className="home">
-      <HeaderContent>
-        <div>
-          <br />
-          <H1>Hi 👋 I'm </H1>
-          <span>
-            <h2 data-text="Niranjan Kumar M">Niranjan Kumar M</h2>
-          </span>
-          <br />
-          <P>
-            An enthusiastic Full Stack Web Developer with a strong set of
-            technical as well as non-technical skills and a dedication towards
-            creating useful and interactive web applications.
-          </P>
-          <HeaderButtons>
-            <A
-              target="_blank"
-              rel="noreferrer"
-              href="/resume/resume"
-            >
-              Resume
-            </A>
-            &nbsp;&nbsp;&nbsp;
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              smooth={true}
-              duration={300}
-            >
-              <A>Projects</A>
-            </Link>
-          </HeaderButtons>
-        </div>
-        <br />
-      </HeaderContent>
-    </Container>
+		<Container className="home">
+			<HeaderContent>
+				<div>
+					<br />
+					<H1>Hi 👋 I'm </H1>
+					<span>
+						<h2 data-text="Niranjan Kumar M">Niranjan Kumar M</h2>
+					</span>
+					<br />
+					<P>
+						An enthusiastic Full Stack Web Developer with a strong
+						set of technical as well as non-technical skills and a
+						dedication towards creating useful and interactive web
+						applications.
+					</P>
+					<HeaderButtons>
+						<A
+							target="_blank"
+							rel="noreferrer"
+							href="https://drive.google.com/file/d/1SDujF1eXc0dtcmvNnM0sMnHTw8o_QD3A/view?usp=sharing"
+						>
+							Resume
+						</A>
+						&nbsp;&nbsp;&nbsp;
+						<Link
+							activeClass="active"
+							to="projects"
+							spy={true}
+							smooth={true}
+							duration={300}
+						>
+							<A>Projects</A>
+						</Link>
+					</HeaderButtons>
+				</div>
+				<br />
+			</HeaderContent>
+		</Container>
   );
 };
 
