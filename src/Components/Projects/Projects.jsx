48b38b09dac8052be71947680c0d683e867cb5f4
Niranjan <narayana9492324464@gmail.com>
2022-03-22 11:44:37 +0530

    Done

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -19,7 +19,7 @@ const Projects = () => {
 			project_name: "tata-Mg1-clone",
 			image: "/project_images/tata1.jpg",
 			project_desc:
-				"This is the clone of TATA 1mg website. A web application to buy medicines and health products. This is our team collaboration project at Full Stack-development bootcamp in Masai School. ",
+				"This is the clone of TATA 1mg website. A web application to buy medicines and health products. This is our team collaboration project at Full Stack-development bootcamp in Masai School.",
 			deploy_link: "https://tata-mg1-clone-amber.vercel.app/",
 			repo_link: "https://github.com/rohan688/tata-Mg1-clone",
 			tech_stack: [
@@ -34,7 +34,8 @@ const Projects = () => {
 			id: 4,
 			project_name: "JIO-Mart- Clone",
 			image: "/project_images/jio1.jpg",
-			project_desc: "https://github.com/jibi966/JioMart-clone",
+			project_desc:
+				"In this project we have tried to make a look alike clone of Jio-Mart Website. With our efforts and the technology stack like JS, CSS and HTML, we were able to clone the front end with similar looks and features.",
 			deploy_link: "https://jio-mart-clone.vercel.app/",
 			repo_link: "https://github.com/jibi966/JioMart-clone",
 			tech_stack: [
